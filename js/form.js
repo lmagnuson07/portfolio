d4f839f4a9316a5dc9dff1da8b39654b58c2db00
lmagnuson07 <logmagns07@gmail.com>
2022-10-30 17:47:57 -0600

    Updated about me desc. Increased about me desc and list font-size. Added some skills. Added Front-End Developer to home page.

---
+++ js/form.js
@@ -0,0 +1,101 @@
+const validate = function () {
+    let result = true;
+    let nameLabel = document.querySelector("[for=name-input]");
+    let emailLabel = document.querySelector("[for=email-input]");
+    let messageLabel = document.querySelector("[for=message-input]");
+
+    let errorMessages = document.querySelectorAll(".contact-error-message");
+    let counter = 0;
+    
+    if (nameLabel.contains(errorMessages[counter])){
+        document.querySelector("[for=name-input] span.contact-error-message").remove();
+        counter++;
+    }
+    if (!document.contact.name.value) {
+        document.contact.name.classList.add("alert");
+        nameLabel.innerHTML += "<span class=\"contact-error-message\">Please enter your name in the field below...</span>";
+        result = false;
+    } else {
+        if (document.contact.name.classList.contains("alert")){
+            document.contact.name.classList.remove("alert");
+        }
+    }
+
+    if (emailLabel.contains(errorMessages[counter])){
+        document.querySelector("[for=email-input] span.contact-error-message").remove();
+        counter++;
+    }
+    if (!document.contact.email.value) {
+        document.contact.email.classList.add("alert");
+        emailLabel.innerHTML += "<span class=\"contact-error-message\">Please enter your email address in the field below...</span>";
+        result = false;
+    } else {
+        if (document.contact.email.classList.contains("alert")){
+            document.contact.email.classList.remove("alert");
+        }
+    }
+
+    if (messageLabel.contains(errorMessages[counter])){
+        document.querySelector("[for=message-input] span.contact-error-message").remove();
+        counter++;
+    }
+    if (!document.contact.message.value) {
+        document.contact.message.classList.add("alert-textarea");
+        messageLabel.innerHTML += "<span class=\"contact-error-message\">Please enter your message (at least 5 characters) in the field below...</span>";
+        result = false;
+    } else {
+        if (document.contact.message.classList.contains("alert-textarea")){
+            document.contact.message.classList.remove("alert-textarea");
+        }
+    }
+    return result;
+}
+
+document.querySelectorAll("input").forEach((input => {
+    input.addEventListener("focus", function () {
+        input.classList.add("valid");
+    });
+}));
+
+document.querySelectorAll("input").forEach((input => {
+    input.addEventListener("blur", function () {
+        if (input.classList.contains("valid")){
+            input.classList.remove("valid");
+        }
+        if (input.classList.contains("alert")){
+            if (input.value.length > 0) {
+                input.classList.remove("alert");
+            }
+        }
+    });
+}));
+
+document.querySelector("#name-input").addEventListener("blur", function (evt) {
+    if (!evt.target.value){
+        evt.target.classList.add("alert");
+    }
+});
+
+document.querySelector("#email-input").addEventListener("blur", function (evt) {
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(evt.target.value)) {
+        evt.target.classList.add("alert");
+    }
+});
+
+document.querySelector("textarea").addEventListener("focus", function (evt) {
+    evt.target.classList.add("valid");
+});
+
+document.querySelector("#message-input").addEventListener("blur", function (evt) {
+    if (evt.target.classList.contains("valid")){
+        evt.target.classList.remove("valid");
+    }
+    if (evt.target.value.length >= 5) {
+        if (evt.target.classList.contains("alert-textarea")){
+            evt.target.classList.remove("alert-textarea");
+        }
+    }
+    else {
+        evt.target.classList.add("alert-textarea");
+    }
+});
