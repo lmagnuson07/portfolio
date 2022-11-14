a4dec9d61e5d725cd7a489dc865ed73b10239384
lmagnuson07 <logmagns07@gmail.com>
2022-11-13 18:56:20 -0700

    Improved the responsivness of the dropdown navigation menu. Added timeouts for some of the class toggles. Toggled the visibility of the nav items.

--- js/main.js
+++ js/main.js
@@ -2,24 +2,66 @@
 let navItems = document.querySelectorAll(".nav-item");
 let socialItems = document.querySelectorAll(".socials-item");
 
+let headerNav = document.querySelector("nav.header-nav");
+let dropdown = document.querySelector(".dropdown");
+
+// hamburger menu
 document.querySelector(".toggle label").addEventListener("click", () => {
-    document.querySelector("nav.header-nav").classList.toggle("toggle-nav");
-    document.querySelector(".dropdown").classList.toggle("toggle-dropdown-height");
-    for (let item of socialItems) {
-        item.classList.toggle("toggle-dropdown");
-    }
+    headerNav.classList.toggle("toggle-nav");
+    dropdown.classList.toggle("toggle-dropdown-height");
+
+    setTimeout(() => {
+        for (let item of navItems) {
+            item.classList.toggle("toggle-visibility");
+        }
+    }, 150);
+    setTimeout(() => {
+        for (let item of socialItems) {
+            item.classList.toggle("toggle-visibility");
+        }
+    }, 300);
+    
     for (let item of navItems) {
        item.classList.toggle("toggle-nav-height"); 
     }
-});
-
-document.querySelector("#dropdownToggle").addEventListener("click", () => {
-    document.querySelector(".dropdown").classList.toggle("toggle-dropdown");
-});
-
-let dropdown = document.querySelector(".dropdown");
-dropdown.addEventListener("mouseleave", () => {
-    if (dropdown.classList.contains("toggle-dropdown")) {
-        dropdown.classList.remove("toggle-dropdown");
+    if (dropdown.classList.contains("toggle-visibility")){
+        dropdown.classList.remove("toggle-visibility");
     }
 });
+
+// arrow beside My Work
+document.querySelector("#dropdownToggle").addEventListener("click", () => {
+    dropdown.classList.toggle("toggle-visibility");
+});
+
+dropdown.addEventListener("mouseleave", () => {
+    if (dropdown.classList.contains("toggle-visibility")) {
+        dropdown.classList.remove("toggle-visibility");
+    }
+});
+
+
+// let navItems = document.querySelectorAll(".nav-item");
+// let socialItems = document.querySelectorAll(".socials-item");
+
+// document.querySelector(".toggle label").addEventListener("click", () => {
+//     document.querySelector("nav.header-nav").classList.toggle("toggle-nav");
+//     document.querySelector(".dropdown").classList.toggle("toggle-dropdown-height");
+//     for (let item of socialItems) {
+//         item.classList.toggle("toggle-dropdown");
+//     }
+//     for (let item of navItems) {
+//        item.classList.toggle("toggle-nav-height"); 
+//     }
+// });
+
+// document.querySelector("#dropdownToggle").addEventListener("click", () => {
+//     document.querySelector(".dropdown").classList.toggle("toggle-dropdown");
+// });
+
+// let dropdown = document.querySelector(".dropdown");
+// dropdown.addEventListener("mouseleave", () => {
+//     if (dropdown.classList.contains("toggle-dropdown")) {
+//         dropdown.classList.remove("toggle-dropdown");
+//     }
+// });
