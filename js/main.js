eafbe53f657cf89bef9a5ad8081a818b948706ac
lmagnuson07 <logmagns07@gmail.com>
2022-11-13 19:18:30 -0700

    Fixed a bug with the dropdown menu positioning. Set max-width to 100%

--- js/main.js
+++ js/main.js
@@ -39,29 +39,3 @@ dropdown.addEventListener("mouseleave", () => {
         dropdown.classList.remove("toggle-visibility");
     }
 });
-
-
-// let navItems = document.querySelectorAll(".nav-item");
-// let socialItems = document.querySelectorAll(".socials-item");
-
-// document.querySelector(".toggle label").addEventListener("click", () => {
-//     document.querySelector("nav.header-nav").classList.toggle("toggle-nav");
-//     document.querySelector(".dropdown").classList.toggle("toggle-dropdown-height");
-//     for (let item of socialItems) {
-//         item.classList.toggle("toggle-dropdown");
-//     }
-//     for (let item of navItems) {
-//        item.classList.toggle("toggle-nav-height"); 
-//     }
-// });
-
-// document.querySelector("#dropdownToggle").addEventListener("click", () => {
-//     document.querySelector(".dropdown").classList.toggle("toggle-dropdown");
-// });
-
-// let dropdown = document.querySelector(".dropdown");
-// dropdown.addEventListener("mouseleave", () => {
-//     if (dropdown.classList.contains("toggle-dropdown")) {
-//         dropdown.classList.remove("toggle-dropdown");
-//     }
-// });
